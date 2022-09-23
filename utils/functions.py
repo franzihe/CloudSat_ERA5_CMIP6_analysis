1dd8bbb8dad8e8424dc7aeb77d60eec491f90723
franzihe <franziska.hellmuth@geo.uio.no>
2022-09-23 16:40:21 +0200

    set values between -45S, 45N to Nan

--- utils/functions.py
+++ utils/functions.py
@@ -809,3 +809,15 @@ def set4D_latitude_values_nan(array, upper_lat, lower_lat):
                                                                                                                   array['latitude'].where(array['latitude']==lower_lat).argmin('latitude').values,:]).coords)
 
     return array
+
+def set3D_latitude_values_nan(array, upper_lat, lower_lat):
+    array[:,array['latitude'].where(array['latitude']==upper_lat).argmin('latitude').values+1:\
+            array['latitude'].where(array['latitude']==lower_lat).argmin('latitude').values,:] = xr.DataArray(data=da.full(shape =(array[:,array['latitude'].where(array['latitude']==upper_lat).argmin('latitude').values+1:\
+                                                                                                                                           array['latitude'].where(array['latitude']==lower_lat).argmin('latitude').values,:]).shape,
+                                                                                                             fill_value=np.nan),
+                                                                                                dims=(array[:,array['latitude'].where(array['latitude']==upper_lat).argmin('latitude').values+1:\
+                                                                                                              array['latitude'].where(array['latitude']==lower_lat).argmin('latitude').values,:]).dims, 
+                                                                                                coords=(array[:,array['latitude'].where(array['latitude']==upper_lat).argmin('latitude').values+1:\
+                                                                                                                array['latitude'].where(array['latitude']==lower_lat).argmin('latitude').values,:]).coords)
+
+    return array
